cdcedd1795f0b9c21107fc1fad979abf86888772
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-06-09 23:54:46 +0300

    fix: Create seperate loops for making the lib

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,6 +1,9 @@
 #/bin/bash
 
 for c_file in *.c; do
-	gcc -c $c_file
-	ar rc liball.a ${c_file%.*}.o
+	gcc -std=gnu89 -Wall -Werror -c $c_file
+done
+
+for o_file in *.o; do
+	ar rc liball.a $o_file
 done
