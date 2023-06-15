8a2da987f5c7350331c508c14e77f262f39051cb
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-06-15 19:19:47 +0300

    chore: Add a crude implementation of "calloc"

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+# include "main.h"
+
+/**
+ * _calloc - A function that allocates memory to "nmemb"
+ * of size "size"
+ * @nmemb: The array that holds the data
+ * @size: The size of the array
+ * Return: arr or void
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int t_len;
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	t_len = nmemb * size;
+
+	arr = malloc(t_len);
+
+	if (arr == NULL)
+		return (NULL);
+
+	return (arr);
+}
