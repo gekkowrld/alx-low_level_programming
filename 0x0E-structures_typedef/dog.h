ee15491b8193c746d5b5f7027d3fc5ec153e1387
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-06-20 13:44:25 +0300

    docs: Add a documentation for dogp

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 # ifndef DOG_H_
 # define DOG_H_
 
-struct dog {
+/**
+ * struct dog - A structure that holds the info of a dog
+ *
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
