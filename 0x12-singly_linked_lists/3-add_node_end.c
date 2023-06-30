6b5bfdc5f10e4dec0e0f083afb6d7f725659a009
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-06-30 23:03:22 +0300

    chore: Add a function that adds a node to the end of a linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+# include "lists.h"
+
+/**
+ * add_node_end - Add a node to the end of a list
+ * @head: The beginning of a linked list
+ * @str: The string that will be added
+ * Return: The newly created list
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node = malloc(sizeof(struct list_s));
+
+	node->str = strdup(str);
+	node->len = strlen(str);
+	node->next = NULL;
+
+	if (*head == NULL)
+		*head = node;
+	else
+	{
+		list_t *temp = *head;
+
+		while (temp->next != NULL)
+			temp = temp->next;
+
+		temp->next = node;
+	}
+
+	return (node);
+}
