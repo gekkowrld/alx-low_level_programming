9fae67e83806f42f874ece7cd67ff7c6c4f22c8f
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-07-11 01:22:30 +0300

    chore: Copy file from source to destination

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * main - Copy file content from file_from to file_to
+ * @argc: The argument count
+ * @argv: The argument string/content
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int f_from, f_to;
+	char *ch = malloc(sizeof(char) * BUFFER);
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	f_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC);
+
+	if (f_to == -1)
+		return (-1);
+
+	if (chmod(argv[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH) == -1)
+		return (-1);
+
+	if (f_to == -1)
+		return (-1);
+
+	f_from = open(argv[1], O_RDONLY);
+
+	if (f_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	read(f_from, ch, BUFFER);
+
+	if (write(f_to, ch, BUFFER) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	close(f_from);
+	close(f_to);
+
+	return (0);
+}
