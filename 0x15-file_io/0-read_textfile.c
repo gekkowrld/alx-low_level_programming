a33016d90ec315d3c30eee386a42027cf8a052a9
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-07-10 23:05:51 +0300

    chore: Read content from file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads input from text file
+ * @filename: The file where the contents are stored
+ * @letters: The character count
+ * Return: The characters read from file
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i = 0;
+	FILE *fd;
+
+	char *ch = malloc(sizeof(char) * letters);
+
+	if (!ch && !filename)
+		return (0);
+
+	fd = fopen(filename, "r");
+
+	if (!fd)
+		return (0);
+
+	i = read(fileno(fd), ch, letters);
+
+	if (i == -1)
+		return (0);
+
+	if (!write(STDOUT_FILENO, ch, i))
+		return (0);
+
+	free(ch);
+
+	fclose(fd);
+
+	return (i);
+
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+# File I/O
+
+File I/O is used to create, modify and delete files.
+As a GNU/Linux says (probably), "Everything is a file".
